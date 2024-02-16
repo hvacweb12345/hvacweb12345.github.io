f75e0e5b2019cca5f6c9b0850ddafc128b8a1151
hightechroofing <140815641+hightechroofing@users.noreply.github.com>
2024-02-16 20:51:06 +0000

    push changes

--- js/script.js
+++ js/script.js
@@ -76,40 +76,5 @@
   });
 
 
-})(jQuery);
-document.addEventListener("DOMContentLoaded", function() {
-  var residentialDropdown = document.getElementById("residentialDropdown");
-  var commercialDropdown = document.getElementById("commercialDropdown");
-  var residentialMenu = document.getElementById("residentialMenu");
-  var commercialMenu = document.getElementById("commercialMenu");
-  var servicesDropdown = document.getElementById("servicesDropdown");
+})
 
-  residentialDropdown.addEventListener("click", function(event) {
-      event.preventDefault();
-      residentialMenu.classList.toggle("show");
-      commercialMenu.classList.remove("show");
-  });
-
-  commercialDropdown.addEventListener("click", function(event) {
-      event.preventDefault();
-      commercialMenu.classList.toggle("show");
-      residentialMenu.classList.remove("show");
-  });
-
-  // Close residential and commercial menus when services dropdown link is clicked
-  servicesDropdown.addEventListener("click", function(event) {
-      if (!residentialMenu.classList.contains("show") && !commercialMenu.classList.contains("show")) {
-          return; // No need to close if neither menu is open
-      }
-      residentialMenu.classList.remove("show");
-      commercialMenu.classList.remove("show");
-  });
-
-  // Close dropdowns when clicking outside
-  document.addEventListener("click", function(event) {
-      if (!event.target.matches('#residentialDropdown') && !event.target.matches('#commercialDropdown')) {
-          residentialMenu.classList.remove("show");
-          commercialMenu.classList.remove("show");
-      }
-  });
-});
